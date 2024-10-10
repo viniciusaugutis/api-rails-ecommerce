a4e699ef81a651d46ba922a89f8e5f70a89a94b6
Vinicius Augutis <vinicius_augutis@hotmail.com>
2024-10-09 22:05:47 -0300

    feat: configure authentication api and controllers

---
+++ test/controllers/admin/v1/api_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Admin::V1::ApiControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/controllers/storefront/v1/api_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Storefront::V1::ApiControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
